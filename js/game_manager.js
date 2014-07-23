54caf0a0ae83f4dbee63de4cbaf3c851bfdc2447
Philip Krachler <e1027173@student.tuwien.ac.at>
2014-07-23 12:49:54 +0200

    prototype for "2048" with dice

--- js/game_manager.js
+++ js/game_manager.js
@@ -167,7 +167,8 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          //changed to 64 for dice values
+          if (merged.value === 64) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
